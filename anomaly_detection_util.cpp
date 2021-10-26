82223ba0170713701a0c63b0bf2bfec58c76e5af
shoval <shovala1000@gmail.com>
2021-10-26 00:13:57 -0700

    final ex1

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -5,118 +5,9 @@
  */
 
 
-#include <iostream>
-#include <math.h>
-#include <cstdio>
+
+#include <cmath>
 #include "anomaly_detection_util.h"
-
-
-/**
- * @param x is a pointer to the number.
- * @param size is the size of x;
- * @return the variance of x.
- */
-float var(float* x, int size){
-
-    // checking the case when the size is 0.
-    if (size == 0) {
-        throw "Division by zero condition";
-    }
-
-    // checking the case when size is not  0;
-    float sum = 0;
-    float powerSum = 0;
-
-    // The loop calculates the regular sum and another sum when each number is in the power of 2.
-    for (int i=0 ; i < size ; i++) {
-        sum += x[i];
-        powerSum += pow(x[i],2);
-    }
-
-    return ((1 / (float)size) * powerSum) - (pow((1 / (float) size) * sum,2));
-}
-
-
-/**
- * @param x  is a pointer to the first number.
- * @param y is a pointer to the second number.
- * @param size is the valid size for x and y.
- * @return the variance between the 2 numbers.
- */
-float cov(float* x, float* y, int size) {
-
-    // checking the case when the size is 0.
-    if (size == 0) {
-        throw "Division by zero condition";
-    }
-
-    // checking the case when the size is not 0.
-    float avgX = avg(x,size);
-    float avgY =avg(y,size);
-
-    float cov = 0;
-
-    // This loop calculates the cov.
-    for (int i=0 ; i < size ; i++) {
-        cov += (x[i]-avgX) * (y[i] - avgY);
-    }
-    return cov / (float) size;
-}
-
-/**
- * @param x  is a pointer to the first number.
- * @param y is a pointer to the second number.
- * @param size is the valid size for x and y.
- * @return the Pearson correlation coefficient of X and Y.
- */
-float pearson(float* x, float* y, int size) {
-
-    // checking the case when the size is 0.
-    if (size == 0) {
-        throw "Division by zero condition";
-    }
-    float v = sqrt((double) var(x,size)) * sqrt((double) var(y,size));
-    if (v == 0) {
-        throw "Division by zero condition";
-    }
-    return (cov(x,y,size)) / (v);
-}
-
-/**
- *
- * @param points is a pointer to array of points.
- * @param size is the array's size.
- * @return  The function performs a linear regression and returns the line equation
- */
-Line linear_reg(Point** points, int size){
-
-    float x [size];
-    float y [size];
-    for (int i=0 ; i < size ; i++){
-        x[i] = points[i]->x;
-        y[i] = points[i]->y;
-    }
-    float varA = var(x,size);
-    if (varA == 0) {
-        return Line ();
-    }
-    float a = cov(x,y,size) / varA;
-    float b = avg(y,size) - a * avg(x,size);
-    return Line(a,b);
-}
-
-/**
- *
- * @param p is a point.
- * @param points is a pointer to array of points.
- * @param size is the array's size.
- * @return  the deviation between point p and the line equation of the points
- */
-float dev(Point p,Point** points, int size){
-    Line line = linear_reg(points,size);
-    return dev(p,line);
-}
-
 
 /**
  * @param x is the x value.
@@ -127,15 +18,6 @@ float getYValueFromLine(float x, Line line) {
     return line.a * x + line.b;
 }
 
-/**
- *
- * @param p is the point.
- * @param l is the line.
- * @return the deviation between point p and the line
- */
-float dev(Point p,Line l){
-    return fabs(getYValueFromLine(p.x,l) - p.y);
-}
 
 /**
  *
@@ -143,19 +25,133 @@ float dev(Point p,Line l){
  * @param size is the array's size.
  * @return the average.
  */
-float avg(float *value,int size){
+float avg(float *value, int size) {
 
-    // checking the case when the size is 0.
-    if (size == 0) {
-        throw "Division by zero condition";
+    // checking the case when the size is 0 or negative.
+    if (size < 1) {
+        throw "invalid division";
     }
-
-    float sumX=0;
-    for (int i=0 ; i < size ; i++) {
+    float sumX = 0;
+    for (int i = 0; i < size; i++) {
         sumX += value[i];
     }
-    return sumX / size;
+    return sumX / (float) size;
 }
+
+/**
+ * @param x is a pointer to the number.
+ * @param size is the size of x;
+ * @return the variance of x.
+ */
+float var(float *x, int size) {
+
+    // checking the case when the size is 0 or negative.
+    if (size < 1) {
+        throw "invalid division";
+    }
+    float powerSum = 0;
+
+    // The loop calculates the sum when each number is in the power of 2.
+    for (int i = 0; i < size; i++) {
+        powerSum += x[i] * x[i];
+    }
+    float avgX = avg(x, size);
+    return (powerSum / (float) size) - (avgX * avgX);
+}
+
+
+/**
+ * @param x  is a pointer to the first number.
+ * @param y is a pointer to the second number.
+ * @param size is the valid size for x and y.
+ * @return the variance between the 2 numbers.
+ */
+float cov(float *x, float *y, int size) {
+
+    // checking the case when the size is 0 or negative.
+    if (size < 1) {
+        throw "invalid division";
+    }
+
+    // checking the case when the size is not 0.
+    float avgX = avg(x, size);
+    float avgY = avg(y, size);
+
+    float sum = 0;
+
+    // This loop calculates the cov.
+    for (int i = 0; i < size; i++) {
+        sum += x[i] * y[i];
+    }
+    return (sum / (float) size) - (avgX * avgY);
+}
+
+/**
+ * @param x  is a pointer to the first number.
+ * @param y is a pointer to the second number.
+ * @param size is the valid size for x and y.
+ * @return the Pearson correlation coefficient of X and Y.
+ */
+float pearson(float *x, float *y, int size) {
+
+    float v = sqrtf(var(x, size)) * sqrtf(var(y, size));
+    // checking the case when the variance is 0.
+    if (v == 0) {
+        throw "Division by zero condition";
+    }
+    return (cov(x, y, size)) / (v);
+}
+
+/**
+ *
+ * @param points is a pointer to array of points.
+ * @param size is the array's size.
+ * @return  The function performs a linear regression and returns the line equation
+ */
+Line linear_reg(Point **points, int size) {
+
+    float x[size];
+    float y[size];
+    for (int i = 0; i < size; i++) {
+        x[i] = points[i]->x;
+        y[i] = points[i]->y;
+    }
+    float varA = var(x, size);
+    // checking the case when the variance is 0.
+    if (varA == 0) {
+        throw "Division by zero condition";
+    }
+    float a = cov(x, y, size) / varA;
+    float b = avg(y, size) - a * avg(x, size);
+    return Line(a, b);
+}
+
+/**
+ *
+ * @param p is the point.
+ * @param l is the line.
+ * @return the deviation between point p and the line
+ */
+float dev(Point p, Line l) {
+    return fabsf(getYValueFromLine(p.x, l) - p.y);
+}
+
+/**
+ *
+ * @param p is a point.
+ * @param points is a pointer to array of points.
+ * @param size is the array's size.
+ * @return  the deviation between point p and the line equation of the points
+ */
+float dev(Point p, Point **points, int size) {
+    Line line = linear_reg(points, size);
+    return dev(p, line);
+}
+
+
+
+
+
 
 
 
