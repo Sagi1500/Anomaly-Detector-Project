2bb43047332009d2b67d31378fd9b70a8b9a2055
shoval <shovala1000@gmail.com>
2021-11-08 00:22:06 -0800

    08.11

---
+++ cmake-build-debug/CMakeFiles/sagishoval.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sagishoval.dir/anomaly_detection_util.cpp.o"
+  "CMakeFiles/sagishoval.dir/mainTrain.cpp.o"
+  "sagishoval"
+  "sagishoval.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sagishoval.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
